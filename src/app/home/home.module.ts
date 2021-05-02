16f2a96d3d83eeb63efe551a8f1b01abba6a037c
masato <masato.ngsm@gmail.com>
2021-05-02 17:25:58 +0900

    パスワードのバリデーションを追加

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
@@ -16,6 +16,7 @@ import { DatePickerPanelComponent } from './components/date-picker-panel/date-pi
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     MatNativeDateModule,
     MatDatepickerModule,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatDatepicker } from '@angular/material/datepicker';
+import { Component } from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,27 +7,47 @@ import { MatDatepicker } from '@angular/material/datepicker';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  earlyShiftDays = {
+  readonly correctHashedPassword =
+    '872e2ead08575be6429990d9784ebf706f47aa99f66ec6579c01f47cf8aed2d2';
+  password = new FormControl();
+  earlyShiftDays: Shift = {
     dates: [],
     label: '早出',
   };
-  dayShiftDays = {
+  dayShiftDays: Shift = {
     dates: [],
     label: '日勤',
   };
-  lateShiftDays = {
+  lateShiftDays: Shift = {
     dates: [],
     label: '遅出',
   };
-  nightShiftDays = {
+  nightShiftDays: Shift = {
     dates: [],
     label: '夜勤',
   };
-  password = '';
-  constructor() {}
+  constructor() {
+    this.password.setAsyncValidators(async (password: AbstractControl) => {
+      const input = password.value;
+      const encoder = new TextEncoder();
+      const hashBuffer = await crypto.subtle.digest(
+        'SHA-256',
+        encoder.encode(input)
+      );
+      const hashArray = Array.from(new Uint8Array(hashBuffer)); // バッファーをバイト列に変換
+      const hashHex = hashArray
+        .map((b) => b.toString(16).padStart(2, '0'))
+        .join('');
+
+      return hashHex === this.correctHashedPassword
+        ? null
+        : { incorrectPassword: 'パスワードが間違っているみたいだよ' };
+    });
+  }
 
   add() {
-    console.log('password:', this.password);
+    console.log('password:', this.password.getError('incorrectPassword'));
+    console.log('invalid', this.password.invalid);
     console.log('shift', this.earlyShiftDays);
   }
 }
